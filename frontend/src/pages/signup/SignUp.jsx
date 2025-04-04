5ba8c9de91475c9289acda55d7b11a8e8214740a
triveni <3veni287@gmail.com>
2025-04-04 18:56:31 +0530

    Initial commit

--- frontend/src/pages/signup/SignUp.jsx
+++ frontend/src/pages/signup/SignUp.jsx
@@ -1,6 +1,6 @@
 import React, { useState } from "react";
 import { useNavigate } from "react-router-dom";
-import "./Signup.css";
+import "./SignUp.css";
 import signUpRightIMG from "../../assets/signUpRight.jpg";
 
 const Signup = () => {
